eb11f392061e7ceb1ec8b24b1608658091eab6a8
daniil <rebi7dan@gmail.com>
2025-09-24 10:07:37 +0400

    version 2

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,76 @@ export default function App() {
           </div>
         </div>
         <div className="price_calculator_container price_calculator-parameters">
-          <div className="price_calculator-parameters_values"></div>
-          <div className="price_calculator-parameters_price">
-              <h3 className="price_title">ИТОГОВАЯ СТОИМОСТЬ</h3>
-              <p className="price_text">
-                Если вы не нашли подходящий вариант — обратитесь к нам по
-                контактам, указанным ниже мы сделаем эскиз индивидуального заказ
+          <div className="price_calculator-parameters_values">
+            <div>
+              <h3 className="values_h3">ВВОД РАЗМЕРОВ ОКНА</h3>
+              <p className="values_text">
+                Укажите точные размеры проёма. Если не знаете — вызовите
+                замерщика бесплатно.
               </p>
+              <div className="values_boxInput">
+                <input
+                  placeholder="Введите высоту, см"
+                  type="text"
+                  inputMode="numeric"
+                  className="values_input"
+                  onInput={(e: React.FormEvent<HTMLInputElement>) => {
+                    const input = e.currentTarget;
+                    let v = input.value.replace(/\D/g, "");
+                    if (v !== "" && Number(v) > 100000) v = "100000";
+                    input.value = v;
+                  }}
+                />
+                <input
+                  placeholder="Введите ширину, см"
+                  type="text"
+                  inputMode="numeric"
+                  className="values_input"
+                  onInput={(e: React.FormEvent<HTMLInputElement>) => {
+                    const input = e.currentTarget;
+                    let v = input.value.replace(/\D/g, "");
+                    if (v !== "" && Number(v) > 100000) v = "100000";
+                    input.value = v;
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <h3 className="values_h3">ВЫБОР ТИПА ИЗДЕЛИЯ</h3>
+              <p className="values_text">
+                Выберите предпочтительный тип жалюзи или штор. Если сомневатесь
+                в выборе — обратитесь к нашему специалисту. Вам помогут с
+                выбором!
+              </p>
+              <input
+                  placeholder="Введите высоту, см"
+                  type="text"
+                  inputMode="numeric"
+                  className="values_input"
+                  onInput={(e: React.FormEvent<HTMLInputElement>) => {
+                    const input = e.currentTarget;
+                    let v = input.value.replace(/\D/g, "");
+                    if (v !== "" && Number(v) > 100000) v = "100000";
+                    input.value = v;
+                  }}
+                />
+            </div>
+            <div>
+              <h3 className="values_h3">МАТЕРИАЛ И ЦВЕТ ПОЛОТНА</h3>
+              <div className="TTTT"></div>
+            </div>
+            <div>
+              <h3 className="values_h3">ЦВЕТ СТЕН</h3>
+              <div className="TTTT"></div>
+            </div>
+          </div>
+
+          <div className="price_calculator-parameters_price">
+            <h3 className="price_title">ИТОГОВАЯ СТОИМОСТЬ</h3>
+            <p className="price_text">
+              Если вы не нашли подходящий вариант — обратитесь к нам по
+              контактам, указанным ниже мы сделаем эскиз индивидуального заказ
+            </p>
             <div className="price_container">
               <div className="price_sum">10 790 ₽</div>
               <button className="price_buttonBasket">
